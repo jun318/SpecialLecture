4f8389722e8fd7672c7cd4501fc2ce83ad17dbcd
Hishikawa Junya <h.junya.20000318@gmail.com>
2022-11-24 23:19:38 +0900

    make github action

---
+++ tests/TestCSVPrinter.py
@@ -0,0 +1,23 @@
+import unittest
+from speciallecture.CSVPrinter import CSVPrinter
+
+class TestCSVPrinter(unittest.TestCase):
+
+    def test_read1(self):
+        printer = CSVPrinter("sample.csv")
+        line = printer.read()
+        print(line)
+        self.assertEqual(3, len(line))
+
+    def test_read2(self):
+        printer = CSVPrinter("sample.csv")
+        line = printer.read()
+        self.assertEqual("value2B", line[1][1])
+
+    def test_read3(self):
+        try:
+            printer = CSVPrinter("nothing.csv")
+            printer.read()
+            unittest.TestCase.fail("This line should not be invoked")
+        except FileNotFoundError as e:
+            pass
